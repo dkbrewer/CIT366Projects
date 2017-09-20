0aae946c9559f9d72067bd4aaabfecabd6d3c0aa
dkbrewer <dantheman1990@comcast.net>
2017-09-20 13:17:42 -0600

    9/20/17 - All files added to repository

---
+++ AllEventsTicketing/Set.js
@@ -0,0 +1,66 @@
+function Set() {
+	
+	
+	this.intersection = function(listA, listB) {
+    
+	   var resultList = [];
+       
+	   if (listA === null || listB === null) {
+	   	return null;
+	   }
+
+	   for (var i = 0; i < listA.length; i++) {
+	   	var nextValue = listA[i];
+
+	   	for (var j = 0; j < listB.length; j++) {
+	   		if (listB[j] === nextValue) {
+	   			resultList.push(listB[j]);
+	   			break;
+			}
+		}
+	   }
+	   return resultList;
+	}
+    
+    
+    
+	this.union = function(listA, listB) {
+
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   
+	   /*-------------------------------Insert your code here -------------------------------------*/ 
+	   
+	   return resultList;
+	}
+
+
+
+
+	this.relativeComplement = function(listA, listB) {
+
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   	   
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}
+
+
+
+	this.symmetricDifference = function(listA, listB) {
+
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}	
+	
+
+}
