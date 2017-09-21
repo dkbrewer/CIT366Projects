ae34ee80b657314eb00c604db2b136acc3f7876a
dkbrewer <dantheman1990@comcast.net>
2017-09-21 15:16:24 -0600

    9/21/17 - All files added to repository

--- AllEventsTicketing/Set.js
+++ AllEventsTicketing/Set.js
@@ -1,10 +1,10 @@
 function Set() {
 	
-	
 	this.intersection = function(listA, listB) {
     
 	   var resultList = [];
-       
+
+        /*-------------------------------Insert your code here -------------------------------------*/
 	   if (listA === null || listB === null) {
 	   	return null;
 	   }
@@ -19,30 +19,50 @@ function Set() {
 			}
 		}
 	   }
+        /*-------------------------------Insert your code here -------------------------------------*/
 	   return resultList;
 	}
-    
-    
-    
+
 	this.union = function(listA, listB) {
 
 	   var resultList = [];
        
 	   /*-------------------------------Insert your code here -------------------------------------*/
-	   
+        if (listA === null || listB === null) {
+            return null;
+        }
+
+        var aList = this.symmetricDifference(listA, listB)
+        var bList = this.intersection(listA, listB)
+
+        resultList = aList.concat(bList);
 	   /*-------------------------------Insert your code here -------------------------------------*/ 
 	   
 	   return resultList;
 	}
-
-
-
 
 	this.relativeComplement = function(listA, listB) {
 
 	   var resultList = [];
        
 	   /*-------------------------------Insert your code here -------------------------------------*/
+        if (listA === null || listB === null) {
+            return null;
+        }
+
+        for (var i = 0; i < listA.length; i++) {
+            var nextValueA = listA[i];
+
+            for (var j = 0; j < listB.length; j++) {
+                var nextValueB = listB[j];
+                if (nextValueA === nextValueB) {
+                    break;
+                }
+            }
+            if (nextValueA !== nextValueB) {
+                resultList.push(nextValueA)
+            }
+        }
 	   	   
 	   /*-------------------------------Insert your code here -------------------------------------*/
        
@@ -56,6 +76,20 @@ function Set() {
 	   var resultList = [];
        
 	   /*-------------------------------Insert your code here -------------------------------------*/
+        if (listA === null || listB === null) {
+            return null;
+        }
+
+        var aList = this.relativeComplement(listA, listB);
+        var bList = this.relativeComplement(listB, listA);
+
+        for (var i = 0; i < aList.length; i++) {
+        	resultList.push(aList[i]);
+		}
+
+        for (var i = 0; i < bList.length; i++) {
+            resultList.push(bList[i]);
+        }
 
 	   /*-------------------------------Insert your code here -------------------------------------*/
        
