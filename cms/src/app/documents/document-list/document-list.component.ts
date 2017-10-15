fb8b17805ee37501e90ac6c649aafa4388da6142
dkbrewer <dantheman1990@comcast.net>
2017-10-14 19:03:23 -0600

    10/7/17 - Lesson 4 upload.

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
-  constructor() { }
+  documents: Document[] = [
+    new Document('1', 'Document 1', 'Document 1', 'www.exampleurl.com'),
+    new Document('2', 'Document 2', 'Document 2', 'www.exampleurl.com'),
+    new Document('3', 'Document 3', 'Document 3', 'www.exampleurl.com'),
+    new Document('4', 'Document 4', 'Document 4', 'www.exampleurl.com')
+  ];
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+  }
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
